59e4bb828ff353c904abdbd3db8a47ef2ea4b1ae
Laura Pérez-Molina <laurapermol@gmail.com>
2023-06-01 12:31:24 +0200

    requirements

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ if [ ! -d "../fit_data" ]; then
 mkdir ../fit_data
 fi
 
-pip install --upgrade pip
+pip  install --upgrade pip
 pip3 install xlrd==1.2.0
+pip3 install plotly
+pip3 install numpy
+pip3 install matplotlib
+pip3 install pandas
