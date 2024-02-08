6d66a861c9551a4201db0ed048e8772c13be99c7
LauPM <laurapermol@gmail.com>
2024-02-08 17:34:50 +0100

    update with lat_height + dictionary with distances

--- lib/sipms_functions.py
+++ lib/sipms_functions.py
@@ -67,33 +67,41 @@ def npy2df(df, per_label=[]):
             mins.append(df[label].min(axis=0))
     return mean, stds, maxs, mins
 
-def data2npy(folder, pcbs_labels, sipm_labels, pins_labels, sipm_number=6, pins_number=8, mode=1, debug=False):
+def data2npy(folder,pcbs_labels,sipm_labels,hlat_labels,pins_labels,sipm_number=6,pins_number=8,mode=1,
+             distances={"pcb_header":3,"anv_header":2,"rev_header":2,"lat_header":4,
+                        "row_pcbs":197,"row_sipm":197,"row_pina":197,"row_pinr":24,"row_hlat":24,
+                        "col_pcbs":11, "col_sipm":14, "col_pina":17, "col_pinr":11,"col_hlat":10}, debug=False):
     '''
     Function to load the data from the .xlsx files into numpy arrays.
-    The configuration is hard-coded, so if you change the name of the files or the location of the data, you will need to change the function.
+    Some configuration is hard-coded, so if you change the name of the files or the location of the data, you will need to change the function.
     Args:
         - folder: folder where the data is stored
         - pcbs_labels: list of the labels of the PCBs
         - sipm_labels: list of the labels of the SiPMs
+        - hlat_labels: list of the labels of the lateral heights
         - pins_labels: list of the labels of the pins
         - sipm_number: number of SiPMs per PCB
         - pins_number: number of pins per PCB
         - mode: number of bunches of 6xSiPMs stored in each .xlsx file
+        - distances: dictionary with the distances between the measurements (i.e. {"pcbs":197,"sipm":8,"pina":5,"pinr":24})
         - debug: if True, it will print the values of the cells in the .xlsx files
     Returns:
+        5 arrays with columns for each variable and rows for each measurement:
         - pcbs_values: numpy array with the values of the PCBs
         - sipm_values: numpy array with the values of the SiPMs
         - pins_values_rev: numpy array with the values of the pins in the back of the PCBs
         - pins_values_anv: numpy array with the values of the pins in the front of the PCBs
+        - hlat_values: numpy array with the values of the lateral heights
     '''
 
     print('\033[93m'+"\nWARNING: the configuration for the xlsx is hard-coded, any change in the naming or how information is distributed in the cells WILL NEED to be CHANGED in the function"+'\033[0m')
     
     names           = os.listdir(folder)
-    pcbs_values     = np.empty([len(names)*mode,              len(pcbs_labels)+1], dtype=object) #solo anverso
-    sipm_values     = np.empty([sipm_number*len(names)*mode,  len(sipm_labels)+1], dtype=object) #solo anverso
-    pins_values_anv = np.empty([pins_number*len(names) *mode, len(pins_labels)+1], dtype=object) #solo anverso
-    pins_values_rev = np.empty([pins_number*len(names) *mode, 2], dtype=object)                #solo reverso/solo altura pines
+    pcbs_values     = np.empty([len(names)*mode,             len(pcbs_labels)+1], dtype=object) #solo anverso
+    sipm_values     = np.empty([sipm_number*len(names)*mode, len(sipm_labels)+1], dtype=object) #solo anverso
+    hlat_values     = np.empty([sipm_number*len(names)*mode, len(hlat_labels)+1], dtype=object) #solo altura pines ESPECIAL FBK
+    pins_values_anv = np.empty([pins_number*len(names)*mode, len(pins_labels)+1], dtype=object) #solo anverso
+    pins_values_rev = np.empty([pins_number*len(names)*mode, 2], dtype=object)                  #solo reverso/solo altura pines
     
     names_sipms = []
     for i in range(sipm_number): names_sipms.append("SiPM #%i"%(i+1))
@@ -106,17 +114,18 @@ def data2npy(folder, pcbs_labels, sipm_labels, pins_labels, sipm_number=6, pins_
     if mode == 1: print("\n[INFO] You have entered \"mode=1\", meaning that each bunch of 6xSiPMs is stored in ONE .xlsx")
     else:         print("\n[INFO] You have entered \"mode=%i\", meaning that in each .xlsx you stored %i bunches of 6xSiPMs"%(mode,mode))
 
-    pcbs_ids = []; sipm_ids = []; pins_anv_ids = []; pins_rev_ids = []
+    pcbs_ids = []; sipm_ids = []; pins_anv_ids = []; pins_rev_ids = []; hlat_ids = []
     for n in np.arange(len(names)): # Distintos archivos --> Placas PCBs
         if debug: print("\n----- LOCATION:", folder + names[n] + '/' + names[n], "-----")
-        ##PREVIOUS CONFIGURATION##
-        # workbook_anv  = xlrd.open_workbook(folder + names[n] + '/' + names[n].replace("Nº","") + '_anverso_1.xlsx')
-        # workbook_rev  = xlrd.open_workbook(folder + names[n] + '/' + names[n].replace("Nº","") + '_reverso_1.xlsx')
         # Open the workbook and define the worksheet:
         workbooks_anv  = xlrd.open_workbook(folder + names[n] + '/' + names[n] + '_anverso.xlsx')
         workbooks_rev  = xlrd.open_workbook(folder + names[n] + '/' + names[n] + '_reverso.xlsx')
         worksheet_anv = workbooks_anv.sheet_by_index(0)
         worksheet_rev = workbooks_rev.sheet_by_index(0)
+        try: 
+            workbooks_lat = xlrd.open_workbook(folder + names[n] + '/' + names[n] + '_alturas.xlsx')
+            worksheet_lat = workbooks_lat.sheet_by_index(0)
+        except: print("No se ha encontrado el archivo de alturas para la placa %s"%names[n])
 
         # Serial number to identify each SiPM #
         serial_number = [];
@@ -128,8 +137,11 @@ def data2npy(folder, pcbs_labels, sipm_labels, pins_labels, sipm_number=6, pins_
         if debug: print('\033[96m'+"\nPCB"+'\033[0m')
         for l in np.arange(len(pcbs_labels)):    # Etiquetas x16
             for m in range(mode):                # Entries in each file
-                pcbs_values[n*mode+m,l] = worksheet_anv.cell(3+l+m*197,11).value #cell allocation in xlsx file
-                if debug: print("row: ", 3+l+m*197, "; value: ", worksheet_anv.cell(3+l+m*197,11).value)
+                row = distances["pcb_header"]+l+m*distances["row_pcbs"]
+                col = distances["col_pcbs"]
+                val = float(worksheet_anv.cell(row,col).value) #cell allocation in xlsx file
+                if debug: print("(row,col): (%i,%i) ;  value: %.2f"%(row,col,val))
+                pcbs_values[n*mode+m,l] = val 
                #pcbs_values[0-(names*mode*#LABELS),0-16] = cell(COLUMNA: 3+l(ETIQUETA(0-16))+m*197(SEPARACION ENTRE MEDIDAS), FILA: 11)
         pcbs_ids.append([str(names[n])+"_ID"+str(sn) for sn in serial_number])
 
@@ -138,18 +150,40 @@ def data2npy(folder, pcbs_labels, sipm_labels, pins_labels, sipm_number=6, pins_
         for l in np.arange(len(sipm_labels)):    # Etiquetas x6
             for m in range(mode): 
                 for j in np.arange(sipm_number): # SiPMs x6
-                    sipm_values[j+m*sipm_number+n*(sipm_number*(mode-1)+sipm_number),l] = worksheet_anv.cell(3+l+j*8+m*197,14).value #cell allocation in xlsx file
-                    if debug: print("row: ", 3+l+j*8+m*197, "; value: ", worksheet_anv.cell(3+l+j*8+m*197,14).value)
+                    row = distances["pcb_header"]+l+j*(distances["anv_header"]+len(sipm_labels))+m*distances["row_sipm"]
+                    col = distances["col_sipm"]
+                    val = float(worksheet_anv.cell(row,col).value) #cell allocation in xlsx file
+                    if debug: print("(row,col): (%i,%i) ;  value: %.2f"%(row,col,val))
+                    sipm_values[j+m*sipm_number+n*(sipm_number*(mode-1)+sipm_number),l] = val
                    #sipm_values[0-(names*mode*#SIPMS*#LABELS),0-6] = cell(COLUMNA: 3+l(ETIQUETA(0-6))+j*8(DISTANCIA ENTRE SIPMS)+m*197(SEPARACION ENTRE MEDIDAS), FILA: 14)
         sipm_ids.append([str(names[n])+"_ID"+str(sn) for sn in [i for i in serial_number for j in range(sipm_number)]])
 
+        # Alturas laterales #
+        if "workbooks_lat" in locals() and hlat_labels != []:
+            print('\033[96m'+"\nLateral heights found! Storing them to plot"+'\033[0m')
+            for l in np.arange(len(hlat_labels)):
+                r = n*(sipm_number*mode)
+                for m in range(mode):
+                    for j in np.arange(sipm_number):
+                        row = distances["lat_header"]+m*distances["row_hlat"]+j
+                        col = distances["col_hlat"] + l
+                        val = float(worksheet_lat.cell(row,col).value) #cell allocation in xlsx file
+                        if debug: print("(row,col): (%i,%i) ;  value: %.2f"%(row,col,val))
+                        hlat_values[r,l] = val
+                        r += 1
+            hlat_ids.append([str(names[n])+"_ID"+str(sn) for sn in [i for i in serial_number for j in range(sipm_number)]])
+        if "workbooks_lat" in locals() and hlat_labels == []: print('\033[91m'+"ERROR: You have entered a file with lateral heights but you have not specified the labels"+'\033[0m')
+        
         # Pins anverso #
         if debug: print('\033[96m'+"\nPins anverso"+'\033[0m')
         for l in np.arange(len(pins_labels)):    # Etiquetas x3
             for m in range(mode):                # Entries in each file
                 for j in np.arange(pins_number): # Pins x8
-                    pins_values_anv[j+m*pins_number+n*(pins_number*(mode-1)+pins_number),l] = worksheet_anv.cell(3+l+j*5+m*197,17).value #cell allocation in xlsx file
-                    if debug: print("row: ", 3+l+j*5+m*197, "; value: ", worksheet_anv.cell(3+l+j*5+m*197,17).value)
+                    row = distances["pcb_header"]+l+j*(distances["anv_header"]+len(pins_labels))+m*distances["row_pina"]
+                    col = distances["col_pina"]
+                    val = float(worksheet_anv.cell(row,col).value) #cell allocation in xlsx file
+                    if debug: print("(row,col): (%i,%i) ;  value: %.2f"%(row,col,val))
+                    pins_values_anv[j+m*pins_number+n*(pins_number*(mode-1)+pins_number),l] = val
                    #pins_values_anv[0-(names*mode**#PINS*#LABELS),0-8] = cell(COLUMNA: 3+l(ETIQUETA(0-3))+j*5(DISTANCIA ENTRE PINS)+m*197(SEPARACION ENTRE MEDIDAS), FILA: 17)
         pins_anv_ids.append([str(names[n])+"_ID"+str(sn) for sn in [i for i in serial_number for j in range(pins_number)]])
 
@@ -157,8 +191,11 @@ def data2npy(folder, pcbs_labels, sipm_labels, pins_labels, sipm_number=6, pins_
         if debug: print('\033[96m'+"\nPins reverso"+'\033[0m')
         for m in range(mode):                    # Entries in each file
             for j in np.arange(pins_number):     # Pins x8
-                pins_values_rev[j+m*pins_number+n*(pins_number*(mode-1)+pins_number),0] = worksheet_rev.cell(3+j+m*24,11).value # Etiqueta x1 (sin loop) REVERSO
-                if debug: print("row: ", 3+j+m*24, "; value: ", worksheet_rev.cell(3+j+m*24,11).value)
+                row = distances["pcb_header"]+j+m*distances["row_pinr"]
+                col = distances["col_pinr"]
+                val = float(worksheet_rev.cell(row,col).value) #cell allocation in xlsx file # Etiqueta x1 (sin loop) REVERSO
+                if debug: print("(row,col): (%i,%i) ;  value: %.2f"%(row,col,val))
+                pins_values_rev[j+m*pins_number+n*(pins_number*(mode-1)+pins_number),0] = val
                #pins_values_rev[0-(names*mode*#PINS)] = cell(COLUMNA: 3+l(ETIQUETA(0-6))+j(0-8 pins)+m*24(SEPARACION ENTRE MEDIDAS), FILA: 11)
         pins_rev_ids.append([str(names[n])+"_ID"+str(sn) for sn in [i for i in serial_number for j in range(pins_number)]])
 
@@ -166,15 +203,17 @@ def data2npy(folder, pcbs_labels, sipm_labels, pins_labels, sipm_number=6, pins_
     sipm_values[:,-1]     = list(np.concatenate(sipm_ids).flat)
     pins_values_anv[:,-1] = list(np.concatenate(pins_anv_ids).flat)
     pins_values_rev[:,-1] = list(np.concatenate(pins_rev_ids).flat)
+    if hlat_labels != []: hlat_values[:,-1] = list(np.concatenate(hlat_ids).flat)
 
     print('\033[96m'+"\nCHECK DIMENSIONS:"+'\033[0m')
     print("Files x Bunches:",len(pcbs_values))
     print("PCBs:",len(pcbs_values))
     print("SiPM:",len(sipm_values))
+    if hlat_labels != []: print("HLAT:",len(hlat_values))
     print("PIN_ANV:",len(pins_values_anv))
     print("PIN_REV:",len(pins_values_rev))
 
-    return pcbs_values, sipm_values, pins_values_rev, pins_values_anv
+    return pcbs_values, sipm_values, pins_values_rev, pins_values_anv, hlat_values
 
 def sanity_check(df_values,values):
     '''
@@ -311,8 +350,8 @@ def plotlytos(title, xlabel, ylabel, df, df_mean, column, colors, decimales=2, t
 
         fig = px.histogram(df, template="presentation", x=column, width=900, height=600, hover_data=[column, "IDs"],text_auto=text_auto)
         histogram_trace = fig.data[0]; hist_values, _ = np.histogram(histogram_trace.x); max_height = np.max(hist_values)
-        vline = 1.4*max_height
-        if "burr" in title: vline = 0.6*max_height
+        vline = 1.8*max_height
+        # if "burr" in title: vline = 0.6*max_height
         
         if text_auto: fig.update_traces(hovertemplate = str(column) + ': %{x}' + '<br>' + "IDs: "+ df["IDs"] + '<extra></extra>')
         fig.add_trace(go.Scatter(x=[None],      y = [None],    mode="lines", line=dict(dash="dash",color="orange", width=0), name="Min: %s mm"%str(np.round(mins,decimales))))
@@ -340,8 +379,8 @@ def plotlytos(title, xlabel, ylabel, df, df_mean, column, colors, decimales=2, t
 
             fig = px.histogram(df.loc[index[col]], x=column, template="presentation",width=900, height=600, hover_data=[df.loc[index[col]][column], df.loc[index[col]]["IDs"]],text_auto=text_auto)
             histogram_trace = fig.data[0]; hist_values, hist_edges = np.histogram(histogram_trace.x); max_height = np.max(hist_values)
-            vline = 1.4*max_height
-            if "burr" in title: vline = 0.6*max_height
+            vline = 1.8*max_height
+            # if "burr" in title: vline = 0.6*max_height
 
             if text_auto: fig.update_traces(hovertemplate = str(column) + ': %{x}' + '<br>' + "IDs: "+ df.loc[index[col]]["IDs"] + '<extra></extra>')
             fig.add_trace(go.Scatter(x=[None],      y = [None],    mode="lines", line=dict(dash="dash",color="orange", width=0), name="Max: %s mm"%str(np.round(maxs,decimales))))
--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ pip3 install plotly
 pip3 install numpy
 pip3 install matplotlib
 pip3 install pandas
+pip3 install kaleido
