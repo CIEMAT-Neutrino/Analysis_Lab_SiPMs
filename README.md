52f9b4d183f1cc79855182c87ab7bc8e79ec7742
LauPM <laurapermol@gmail.com>
2024-03-20 15:39:00 +0100

    clean structure

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Analysis_Lab_SiPMs
-Repository with the notebooks used to analyse the data extracted from the SiPMs dimensions. Also some tips on how to plot IR02 control parameters.
+Repository with the notebooks used to analyze the data extracted from the SiPMs dimensions. Also some tips on how to plot IR02 control parameters.
 
 Expected structure of the repository:
   * data: data extracted from the SiPMs (i.e .xlsx files)                  [included in the .gitignore you will only see your local files]
-  * fit_data: grouped data from the SiPMs + statistics (i.e .txt files)    [included in the .gitignore you will only see your local files]
-  * notebooks: notebooks used to analyse the data
+  * src: code to be executed:
+    - notebooks: notebooks used to analyze the data
+    - *.py macros to run the analysis from the terminal (individual.py runs on a selected folder and grouped.py runs on all HPK/FBK labeled folders)
+    - folder_config*: yml file is use to load/classify easily the folders inside the macros (txt have additional information of unused folders)
   * lib: libraries with the used functions
+
+When your data is analyzed you will get two new folders:
+  * fit_data: grouped data from the SiPMs + statistics (i.e .txt files)    [included in the .gitignore you will only see your local files] 
+  * images: all the variables plots 
 
 
 RUN:
@@ -15,10 +21,12 @@ sh setup.sh
 ```
 to install packages and create fit_data (data folder is assumed to exist, if not create it)
 
-Open the notebooks and run the cells. You can save the output in the fit_data folder and you will get histograms at the end of the notebook with the information.
+Option 1) Open the notebooks and run the cells. You can save the output in the fit_data folder and you will get histograms at the end of the notebook with the information.
 
-If you are looking to some data to test the code you can find some in:
-- **/pnfs/ciemat.es/data/neutrinos/FD1_SiPMsTests_IR02/** there are two folder HPK and FBK with the data produced by the mechanical measurements performed in the IR02. Additional information may be found in the neutrino drive ANALISIS LAB/FD1_SiPMsTests (IR02) with summaries and presentations of the data.
+Option 2) [from src folder] Run `python3 individual.py` to get the results on an individual set of data (errors and images will be generated on its own folder) and `python3 grouped.py` to get all HPK/FBK errors and plots of all the SiPMs classified as HPK/FBK.
+
+If you are looking for some data to test the code you can find some in the:
+- **/pnfs/ciemat.es/data/neutrinos/LAB/2023_03_08_DUNE-HD_SiPM_MECHANICAL_MEASUREMENTS/**: there are two folders HPK and FBK with the data produced by the mechanical measurements performed in the IR02. The files used with the Specifications for both SiPMs are also here. Additional information may be found in the neutrino drive `LAB/2023_03_08_DUNE-HD_SiPM_MECHANICAL_MEASUREMENTS` with summaries and presentations of the data.
 
 
 ## LICENSE
@@ -26,4 +34,4 @@ If you are looking to some data to test the code you can find some in:
 
 ## Authors (alphabetical order, please insert your name here if you contribute to this project)
 
-* [**Pérez-Molina, Laura**](https://github.com/rodralva)
+* [**Pérez-Molina, Laura**](https://github.com/LauPM)
--- src/grouped.py
+++ src/grouped.py
@@ -2,14 +2,16 @@ import sys; sys.path.insert(0, '../'); from lib import *
 
 choices = ['HPK', 'FBK']
 question = [inquirer.List('sipm', message="Choose your SiPMs", choices=choices)]
+answers = inquirer.prompt(question)
+chosen_sipm = answers['sipm']
 
 show_plots = [inquirer.List('show_plots', message="Do you want to show the plots?",choices=['Yes', 'No'],default='No')]
 answer = inquirer.prompt(show_plots)
 show_plots = answer['show_plots'][0].lower() in ['true', '1', 't', 'y', 'yes']
 
-# Prompt the user to choose a SiPM
-answers = inquirer.prompt(question)
-chosen_sipm = answers['sipm']
+write_xlsx = [inquirer.List('write_xlsx', message="Do you want to save the grouped outputs in a XLSX file?",choices=['Yes', 'No'],default='Yes')]
+xlsx = inquirer.prompt(write_xlsx)
+write_xlsx = xlsx['write_xlsx'][0].lower() in ['true', '1', 't', 'y', 'yes']
 
 with open('folder_config.yml', 'r') as file:
     folder_config = yaml.safe_load(file)
@@ -237,3 +239,92 @@ if hlat_labels != []:
                         save=True,save_path="../data/"+chosen_sipm+"/images/")
         if show_plots: fig.show()
         fig.write_image("../data/"+chosen_sipm+"/images/"+str(titles[i])+".png")
+
+
+
+if write_xlsx:
+    print("\n------------------------------------------------------------\n")
+    print("WRITING XLSX")
+    from openpyxl import Workbook
+    chosen_folder = "../data/" + chosen_sipm
+    print("Chosen folder:", chosen_folder)
+
+    excel_writer = pd.ExcelWriter(chosen_folder+'/summary_'+chosen_sipm+'.xlsx', engine='openpyxl')
+    fit_data_folder = os.path.join(chosen_folder, 'fit_data')
+    txt_files = [f for f in os.listdir(fit_data_folder) if f.endswith('.txt')]
+    if chosen_sipm == 'HPK':
+        config = {"df_pcbs":{
+                            "headers":["Diameter_(S1)","Y_Position_(S1)","X_Position_(S1)","Diameter_(S2)","Y_Position_(S6)","X_Position_(S6)",
+                            "Width","Length","Burr_length_(R1-rigth)","Burr_width_(R2-left)","Burr_length_(R1-rigth)","Burr_width_(R2-rigth)",
+                            "Distance_drills","ID","DATE_END_ANALYSIS"],
+                            "drop":[6,7,9]
+                            },
+                    "df_sipm":{
+                            "headers":["Position","Width","Length","X_Position","Y_Position","Heigth","ID","DATE_END_ANALYSIS"],
+                            "drop":[4]
+                            },
+                    "df_pina":{
+                            "headers":["Position","X_Position","Y_Position","Heigth","ID","DATE_END_ANALYSIS"],
+                            "drop":[]
+                            },
+                    "df_pinr":{
+                            "headers":["Position","Heigth","ID","DATE_END_ANALYSIS"],
+                            "drop":[]
+                            }
+                    }
+        
+    if chosen_sipm == 'FBK':
+        config = {"df_pcbs":{
+                            "headers":["Diameter_(S1)","Y_Position_(S1)","X_Position_(S1)","Diameter_(S2)","Y_Position_(S6)","X_Position_(S6)",
+                            "Width","Length","Burr_length_(R1-rigth)","Burr_width_(R2-left)","Burr_length_(R1-rigth)","Burr_width_(R2-rigth)",
+                            "Distance_drills","ID","DATE_END_ANALYSIS"],
+                            "drop":[6,7,9]
+                            },
+                    "df_sipm":{
+                            "headers":["Position","Width","Length","X_Position","Y_Position","Heigth",
+                                        'Box_width', 'Box_length','Box_X_Position', 'Box_Y_Position',
+                                        "ID","DATE_END_ANALYSIS"],
+                            "drop":[4] # AQUI HAY MAS INFO 
+                            },
+                    "df_pina":{
+                            "headers":["Position","X_Position","Y_Position","Heigth","ID","DATE_END_ANALYSIS"],
+                            "drop":[]
+                            },
+                    "df_pinr":{
+                            "headers":["Position","Heigth","ID","DATE_END_ANALYSIS"],
+                            "drop":[]
+                            },
+                    "df_hlat":{
+                            "headers":["Position","Front_height_(mean)","Front_height_(max)","Back_height_(mean)","Back_height_(max)",
+                                        "ID","DATE_END_ANALYSIS"],
+                            "drop":[]
+                            }
+                    }
+
+
+    # Loop over each text file
+    for txt_file in txt_files:
+        if "errors" not in txt_file: 
+            print("Processing", txt_file)
+            name = str(txt_file.replace(".txt",""))
+            df = pd.read_csv(os.path.join(fit_data_folder, txt_file),header=None,sep="\t")
+            df = df.drop(df.index[0])
+            if "df_pcbs" in name: df.drop(df.columns[0], axis=1, inplace=True)
+            df = df.apply(pd.to_numeric, errors='ignore')
+            # print("BEFORE\n",df)
+            df = df.dropna(axis=1)
+            if config[name]["drop"] != []:
+                for col in config[name]["drop"]:
+                    # print("DROPPING",col)
+                    df = df.drop(df.columns[col], axis=1)
+            # print("HEADERS",len(config[name]["headers"]))
+            # print(len(df.columns))
+            df.columns = config[name]["headers"]
+            df = df.round(3)
+            df = df.sort_values(by=[df.columns[-2], df.columns[-1]])
+            # print("AFTER\n",df)
+            df.to_excel(excel_writer, sheet_name=os.path.splitext(txt_file)[0], index=False)
+    excel_writer.save()
+    excel_writer.close()
+    print('\033[92m'+"XLSX written in", chosen_folder+'/summary_'+chosen_sipm+'.xlsx'+'\033[0m')
+    print("\n------------------------------------------------------------")
