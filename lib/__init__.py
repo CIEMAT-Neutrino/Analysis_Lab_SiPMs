d01c2d602d3376e69f2525acb06bdaf06c427dc4
LauPM <laurapermol@gmail.com>
2024-03-20 12:52:00 +0100

    cositas bonitas

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,6 +3,6 @@ import os
 import numpy             as np
 import matplotlib.pyplot as plt
 import pandas            as pd
-import xlrd, csv
+import xlrd, csv, inquirer,yaml,gc
 
 from .sipms_functions import *
